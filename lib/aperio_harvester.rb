aa5344219c2218a57af85c0e8b255e2280024460
Sean Lin <seanl@intersect.org.au>
2012-12-04 10:48:48 +1100

    UNSWDC5H-451: Updates are currently not being transmitted from APERIO to ACData when the user tags in APERIO to Update ACData
    Fix some column mapping issus between aperio and acdata

--- lib/aperio_harvester.rb
+++ lib/aperio_harvester.rb
@@ -98,7 +98,7 @@ class AperioHarvester
 
   def process_slide(slide_data)
     # Specimens in Aperio are samples in AC DATA
-    sample_id = slide_data["Specimen ID"].to_i
+    sample_id = slide_data["User Specimen ID"].to_i
     project_id = slide_data["ACData ID"].to_i
 
     project = Project.find(project_id) if project_id > 0
@@ -158,6 +158,10 @@ class AperioHarvester
 
     # Create dataset if it does not exist in the database
     unless dataset
+      # There is no "File Name", we extract it from path
+      path = slide_data["File Location"]
+      slide_data["File Name"] = /.*\\(.*)/.match(path)[1]
+
       dataset = sample.datasets.new(:name => slide_data["File Name"],
                                     :external_data_source => 'Aperio',
                                     :external_id => slide_id,
@@ -167,8 +171,8 @@ class AperioHarvester
 
     dataset.metadata_values.clear
 
-    add_thumbnail(:slide, dataset, slide_data["Image ID"].to_i)
-    add_thumbnail(:label, dataset, slide_data["Image ID"].to_i)
+    add_thumbnail('slide', dataset, slide_data["Image ID"].to_i)
+    add_thumbnail('label', dataset, slide_data["Image ID"].to_i)
 
     dataset.metadata_values.create!(:key => 'ID', :value => slide_data["Slide ID"], :core => true, :supplied => false)
     dataset.metadata_values.create!(:key => 'Block ID', :value => slide_data["Block ID"], :core => true, :supplied => false)
