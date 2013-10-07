9757d3560c5d0200f2bbde3ed6eeab06a4e73774
Sean Lin <seanl@intersect.org.au>
2013-10-08 09:28:15 +1100

    Fix failed cucumber features

--- spec/factories/instruments.rb
+++ spec/factories/instruments.rb
@@ -5,5 +5,5 @@ Factory.define :instrument do |f|
   f.association :instrument_rule, :factory => :instrument_rule
   f.instrument_class "Raman"
   f.is_available true
-  f.instrument_file_types { |ft| [ft.association(:instrument_file_type)] }
+  #f.instrument_file_types { |ft| [ft.association(:instrument_file_type)] }
 end
